03a850a2bc29df1941ba9382df682dfbe9c24c04
boren.fu <boren.fu@nextfortune.tw>
2023-11-25 16:44:09 +0800

    test: same day and same month

--- TddBudget/UnitTest1.cs
+++ TddBudget/UnitTest1.cs
@@ -1,15 +1,42 @@
+using NSubstitute;
+
 namespace TddBudget;
 
 public class Tests
 {
+    private BudgetService _budgetService;
+    private IBudgetRepo _budgetRepo;
+
     [SetUp]
     public void Setup()
     {
+        _budgetRepo = Substitute.For<IBudgetRepo>();
+        _budgetService = new BudgetService(_budgetRepo);
     }
 
     [Test]
-    public void Test1()
+    public void sameday()
     {
-        Assert.Pass();
+        _budgetRepo.GetAll().Returns(new List<Budget>()
+        {
+            new Budget() { YearMonth = "202311", Amount = 31 }
+        });
+
+        var actual = _budgetService.Query(new DateTime(2023,11,25), new DateTime(2023,11,25));
+        Assert.AreEqual(1,actual);
+            
+    }
+    
+    [Test]
+    public void samemonth()
+    {
+        _budgetRepo.GetAll().Returns(new List<Budget>()
+        {
+            new Budget() { YearMonth = "202311", Amount = 31 }
+        });
+
+        var actual = _budgetService.Query(new DateTime(2023,11,25), new DateTime(2023,11,27));
+        Assert.AreEqual(3,actual);
+            
     }
 }
